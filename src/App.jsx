31c92270b416d22dec09f0047ee10196624497d4
Kyungin Na <nakyungin93@gmail.com>
2020-12-18 15:39:53 +0100

    Hello world from application state - version 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { useSelector } from "react-redux";
 
 const App = () => {
-  const [message, setMessage] = useState({ greeting: 'Hello world from hooked state using an object' })
+  // const [message, setMessage] = useState({ greeting: 'Hello world from hooked state using an object' })
   // let message = 'Hello World from variable'
+  const greeting = useSelector(state => state.greeting)
   return (
     <>
-      <h1>{message.greeting}</h1>
+      <h1>{greeting}</h1>
     </>
   )
 }
